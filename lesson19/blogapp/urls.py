6405a847611623d3410b91f65a429332095a695f
VyacheslawDekin <vyacheslaw.dekin@yandex.ru>
2022-07-03 00:09:57 +0300

    urls

---
+++ lesson19/blogapp/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from django.contrib.auth.views import LogoutView, LoginView
+from . import views
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('login/', LoginView.as_view(template_name='login/login.html'), name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
+    path('create/', views.create_post, name='create_post'),
+    path('edit/<int:post_id>/', views.edit_post, name='edit_post'),
+    path('post/<int:post_id>/', views.show_post, name='show_post'),
+    path('delete/<int:post_id>/', views.delete_post, name='delete_post'),
+    path('profile/', views.profile, name='profile'),
+]
